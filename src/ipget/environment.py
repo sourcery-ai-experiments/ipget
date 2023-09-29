7562b8c0b55102b29295ef3c27d16ff42359f574
Andrew Kinsman <69199502+LunaPurpleSunshine@users.noreply.github.com>
2023-09-30 00:20:44 +0100

    Add environment.py.
    This is the source of truth for environment variables used in the app.

---
+++ src/ipget/environment.py
@@ -0,0 +1,22 @@
+###########################################################
+#                  Environment Variables                  #
+# This is the single source of truth for env var names
+###########################################################
+# LoggerSettings
+LOG_LEVEL_ENV = "IPGET_LOG_LEVEL"
+LOG_FILE_PATH = "IPGET_LOG_FILE_PATH"
+# HealthcheckSettings
+HEALTHCHECK_SERVER_ENV = "IPGET_HEALTHCHECK_SERVER"
+HEALTHCHECK_UUID_ENV = "IPGET_HEALTHCHECK_UUID"
+# SQLiteDatabaseSettings
+SQLITE_DATABASE_PATH_ENV = "IPGET_DATABASE"
+# GenericDatabaseSettings
+GENERIC_DB_USERNAME_ENV = "IPGET_USERNAME"
+GENERIC_DB_PASSWORD_ENV = "IPGET_PASSWORD"
+GENERIC_DB_HOST_ENV = "IPGET_HOST"
+GENERIC_DB_PORT_ENV = "IPGET_PORT"
+GENERIC_DB_DATABASE_NAME_ENV = "IPGET_DATABASE"
+# AppSettings
+DISCORD_WEBHOOK_ENV = "IPGET_DISCORD_WEBHOOK"
+DATABASE_TYPE_ENV = "IPGET_DB_TYPE"
+###########################################################
